854d8d596cfd6205a297d84f89dac8c211de579d
Albert Barsamov <abarsamov@worldia.com>
2023-10-22 00:05:50 +0200

    add local moving logic

--- src/Game/MovingElements/Ghosts.tsx
+++ src/Game/MovingElements/Ghosts.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Sprite } from "@pixi/react";
 import GameContext from "../../context/game-context";
 
@@ -6,13 +6,28 @@ const Ghosts = () => {
   const ghostNum = 6;
   const topScreenStartPoint = 14;
   const interGhostSpacing = 40;
-  const ghosts = Array.from({ length: ghostNum }, (_, index) => (
+  const initialX = window.innerWidth;
+  const [xPositions, setXPositions] = useState(
+    Array.from({ length: ghostNum }, () => initialX)
+  );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update x positions for each ghost
+      setXPositions((prevXPositions) => prevXPositions.map((x) => x - 1));
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const ghosts = xPositions.map((x, index) => (
     <Sprite
-      key={index} // Add a unique key prop for each element
+      key={index}
       image="/images/ghost.svg"
       anchor={1}
       scale={{ x: 0.5, y: 0.5 }}
-      x={window.innerWidth}
+      x={x}
       y={window.innerHeight / topScreenStartPoint + index * interGhostSpacing}
     />
   ));
