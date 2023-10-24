284f103eaa853479acd4229f17252e8f1378a186
jilink <jimmyfac1@gmail.com>
2023-10-24 14:16:17 +0200

    quick fixes

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Pumpkins } from "./MovingElements/Pumpkin";
 import Ghosts from "./MovingElements/Ghosts";
-import Candy from "./MovingElements/Candy";
 
 const Game = () => {
   return (
     <>
       <Ghosts />
       <Pumpkins />
-      <Candy/>
     </>
   );
 };
