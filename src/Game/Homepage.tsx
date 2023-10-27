9775c4b1fbb76c94d30e36d519c3f60f1f53ec9a
Albert Barsamov <abarsamov@worldia.com>
2023-10-27 17:42:36 +0200

    fix: homepage update + css fix

--- src/Game/Homepage.tsx
+++ src/Game/Homepage.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useContext, useEffect, useState } from "react";
 import { PlayerId } from "rune-games-sdk";
 import useSound from "../hooks/useSounds";
-
+import AnimatedBat from "../../public/images/giphy.webp";
 type Props = {
   id: PlayerId;
 };
@@ -20,33 +20,24 @@ const Homepage: FC<Props> = ({ id }) => {
     setWait(true);
   };
   return (
-    <>
-      <img
-        className="animated_bat"
-        src="https://media1.giphy.com/media/0xR7MUO0hJfWtco7C6/giphy.gif"
-      />
-
-      <main className="homepage_main">
-        <div>
-          <h1 className="homepage_title">Flapp'invader</h1>
-        </div>
-        <div>
-          {!wait ? (
-            <button onClick={handleStart} className="homepage_btn">
-              Start
-            </button>
-          ) : (
-            <h2 className="homepage_waiting_title">
-              Waiting for your teammates...
-            </h2>
-          )}
-        </div>
-      </main>
-      <img
-        className="animated_bat second"
-        src="https://media1.giphy.com/media/0xR7MUO0hJfWtco7C6/giphy.gif"
-      />
-    </>
+    <main className="homepage_main">
+      <img className="animated_bat" src={AnimatedBat} />
+      <div>
+        <h1 className="homepage_title">Flapp'invader</h1>
+      </div>
+      <div>
+        {!wait ? (
+          <button onClick={handleStart} className="homepage_btn">
+            Start
+          </button>
+        ) : (
+          <h2 className="homepage_waiting_title">
+            Waiting for your teammates...
+          </h2>
+        )}
+      </div>
+      <img className="animated_bat second" src={AnimatedBat} />
+    </main>
   );
 };
 
